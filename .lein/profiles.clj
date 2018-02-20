93b3b9ac633806b47295c3ceb63616c54f2ec7b4
Daniel Dinnyes <ddinnyes@gmail.com>
2018-02-20 20:11:00 +0000

    added leiningen profiles.clj

---
+++ .lein/profiles.clj
@@ -0,0 +1,19 @@
+{:user
+ {:plugins
+  [[cider/cider-nrepl "0.15.1"]
+   [com.billpiel/sayid "0.0.15"]
+   [lein-ring "0.12.1"]
+   [lein-ancient "0.6.15"]
+   [com.jakemccrary/lein-test-refresh "0.21.1"]]
+  :dependencies
+  [[org.clojure/tools.nrepl "0.2.13"]]}
+ :dev
+ {:dependencies
+  [[org.clojure/test.check "0.9.0"]
+   ;; utils
+   [com.taoensso/timbre "4.10.0"]
+   [cheshire "5.8.0"]
+   [clj-time "0.14.0"]
+   [clojurewerkz/money "1.10.0"]
+   [com.stuartsierra/component "0.3.2"]]}}
+
