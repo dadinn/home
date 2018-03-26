548c552d33d5316311e75ec16bcdf0ee556648f6
Daniel Dinnyes <ddinnyes@gmail.com>
2018-03-26 15:13:45 +0100

    added lein-bin plugin to leiningen profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,6 +2,7 @@
  {:plugins
   [[cider/cider-nrepl "0.15.1"]
    [com.billpiel/sayid "0.0.15"]
+   [lein-bin "0.3.5"]
    [lein-ring "0.12.1"]
    [lein-ancient "0.6.15"]
    [com.jakemccrary/lein-test-refresh "0.21.1"]]}
