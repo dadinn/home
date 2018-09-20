55edd9a08f319b628932794e28c9db27f2c06fc2
Daniel Dinnyes <ddinnyes@gmail.com>
2018-09-20 16:43:14 +0100

    updated .lein/profiles.clj

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,18 +1,18 @@
 {:user
  {:plugins
-  [[cider/cider-nrepl "0.15.1"]
-   [com.billpiel/sayid "0.0.15"]
+  [[cider/cider-nrepl "0.18.0"]
+   [com.billpiel/sayid "0.0.17"]
    [lein-bin "0.3.5"]
-   [lein-ring "0.12.1"]
+   [lein-ring "0.12.4"]
    [lein-ancient "0.6.15"]
-   [com.jakemccrary/lein-test-refresh "0.21.1"]]}
+   [com.jakemccrary/lein-test-refresh "0.23.0"]]}
  :dev
  {:dependencies
   [[org.clojure/test.check "0.9.0"]
    [org.clojure/tools.nrepl "0.2.13"]
    ;; utils
    [cheshire "5.8.0"]
-   [clj-time "0.14.0"]
+   [clj-time "0.14.4"]
    [clojurewerkz/money "1.10.0"]
    [com.taoensso/timbre "4.10.0"]
    [com.stuartsierra/component "0.3.2"]]}
