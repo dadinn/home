ee1541588a04d041c359b53250093daa9ad36ef8
Daniel Dinnyes <ddinnyes@gmail.com>
2019-02-10 21:53:52 +0000

    removed :dev profile with :dependencies
    can cause conflicts, or might be missed out of project dependencies

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -6,16 +6,6 @@
    [lein-ring "0.12.4"]
    [lein-ancient "0.6.15"]
    [com.jakemccrary/lein-test-refresh "0.23.0"]]}
- :dev
- {:dependencies
-  [[org.clojure/test.check "0.9.0"]
-   [org.clojure/tools.nrepl "0.2.13"]
-   ;; utils
-   [cheshire "5.8.0"]
-   [clj-time "0.14.4"]
-   [clojurewerkz/money "1.10.0"]
-   [com.taoensso/timbre "4.10.0"]
-   [com.stuartsierra/component "0.3.2"]]}
  :uberjar
  {:aot :all
   :pedantic? :abort}}
