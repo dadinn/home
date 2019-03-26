1d0a719ef7adf644422f73aea24bd1545de49197
Daniel Dinnyes <ddinnyes@gmail.com>
2019-03-26 23:55:53 +0000

    removed cider/cider-nrepl from plugins
    Newer versions of the cider emacs package fetch the correct version automatically

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,14 +1,13 @@
 {:user
  {:plugins
-  [[cider/cider-nrepl "0.21.0"]
+  [
    [com.billpiel/sayid "0.0.17"]
    [lein-bin "0.3.5"]
    [lein-ring "0.12.4"]
    [lein-ancient "0.6.15"]
    [com.jakemccrary/lein-test-refresh "0.23.0"]]
   :middleware
-  [cider-nrepl.plugin/middleware
-   sayid.plugin/middleware]}
+  [sayid.plugin/middleware]}
  :uberjar
  {:aot :all
   :pedantic? :abort}}
