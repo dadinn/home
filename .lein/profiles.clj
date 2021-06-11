a51b0f565c146bc4051c2f9662741b04ad3642ea
Daniel Dinnyes <dadinn@besenczy.net>
2021-06-11 11:11:00 +0100

    added lein-auto plugin
    Allows running unit tests automatically on file changes.

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -4,6 +4,7 @@
    [com.billpiel/sayid "0.1.0"]
    [lein-binplus "0.6.5"]
    [lein-ring "0.12.4"]
+   [lein-auto "0.1.3"]
    [lein-ancient "0.6.15"]
    [com.jakemccrary/lein-test-refresh "0.23.0"]]
   :middleware
